77c60d8d2d0155cff1184351720ad8ea699c38ed
Tai Minh <minhtainth06111999@gmail.com>
2024-03-09 17:04:45 +0700

    demo-project-1

--- src/ErrorAppComponent/ErrorApp.jsx
+++ src/ErrorAppComponent/ErrorApp.jsx
@@ -1,15 +1,12 @@
 import { useRouteError } from "react-router-dom";
-// import ErrorAppCSS from "./ErrorApp.module.css"
+import ErrorAppCSS from "./ErrorApp.module.css";
 export default function ErrorApp() {
     const error = useRouteError();
     return (
         <>
-            <div>
-                <p>The page you&apos;re looking for doesn&apos;t exist.</p>
-                <p>
-                    <i>Page {error.statusText ? error.statusText : error.message}</i>
-                </p>
-            </div >
+            <div className={ErrorAppCSS.div}>
+                <p>Page {error.statusText ? error.statusText : error.message}</p>
+            </div>
         </>
     );
 }
