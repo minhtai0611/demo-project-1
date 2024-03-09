575e0bc8b389176f555bdeb34883b1022f742388
Tai Minh <minhtainth06111999@gmail.com>
2024-03-09 16:03:48 +0700

    demo-project-1

--- src/ErrorAppComponent/ErrorApp.jsx
+++ src/ErrorAppComponent/ErrorApp.jsx
@@ -1,14 +1,15 @@
 import { useRouteError } from "react-router-dom";
+// import ErrorAppCSS from "./ErrorApp.module.css"
 export default function ErrorApp() {
     const error = useRouteError();
     return (
         <>
             <div>
-                <p>Sorry, you navigated to routes that do not exist</p>
+                <p>The page you&apos;re looking for doesn&apos;t exist.</p>
                 <p>
-                    <i>{error.statusText ? error.statusText : error.message}</i>
+                    <i>Page {error.statusText ? error.statusText : error.message}</i>
                 </p>
-            </div>
+            </div >
         </>
     );
 }
--- src/HeaderComponent/Header.jsx
+++ src/HeaderComponent/Header.jsx
@@ -3,32 +3,32 @@ import HeaderCSS from "./Header.module.css";
 export default function Header() {
     return (
         <>
-            <header>
-                <a href="#">
-                    <img src={LogoBook} alt="LogoBook" className={HeaderCSS.img} />
+
+            <header className={HeaderCSS.header + " " + HeaderCSS.all}>
+                <a href="#" className={HeaderCSS.a}>
+                    <img src={LogoBook} alt="LogoBook" className={HeaderCSS.headerImg} />
                 </a>
-                <nav>
-                    <input
+                <nav className={HeaderCSS.headerNavAll}>
+                    <input className={HeaderCSS.headerNavAll + " " + HeaderCSS.headerInput + " " + HeaderCSS.all}
                         type="search"
                         name="searchBook"
                         placeholder="Search ebooks, magazines and more..."
                         maxLength="50"
                     />
-                    <ul>
-                        <li>
-                            <a href="#about">
-                                About
-                            </a>
+                    <ul className={HeaderCSS.headerNavAll + " " + HeaderCSS.ul}>
+                        <li className={HeaderCSS.headerLi + " " + HeaderCSS.headerNavAll}>
+                            <a href="#about" className={HeaderCSS.a + " " + HeaderCSS.all + " " + HeaderCSS.headerNavAll}>About</a>
                         </li>
-                        <li>
-                            <a href="#price">Pricing</a>
+                        <li className={HeaderCSS.headerLi + " " + HeaderCSS.headerNavAll}>
+                            <a href="#price" className={HeaderCSS.a + " " + HeaderCSS.all + " " + HeaderCSS.headerNavAll}>Pricing</a>
                         </li>
-                        <li>
-                            <a href="#contact">Contact</a>
+                        <li className={HeaderCSS.headerLi + " " + HeaderCSS.headerNavAll}>
+                            <a href="#contact" className={HeaderCSS.a + " " + HeaderCSS.all + " " + HeaderCSS.headerNavAll}>Contact</a>
                         </li>
                     </ul>
                 </nav>
             </header>
+
         </>
     );
 }
