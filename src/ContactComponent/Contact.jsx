84f2e620a0b4e8128c481409b9a1cbca673cc04e
Tai Minh <minhtainth06111999@gmail.com>
2024-03-12 16:56:17 +0700

    demo-project-1

--- src/ContactComponent/Contact.jsx
+++ src/ContactComponent/Contact.jsx
@@ -2,83 +2,122 @@ import styled from "./Contact.module.css";
 export default function Contact() {
     return (
         <>
-            <div className={styled.all}>
+            <section className={styled.all}>
                 <p>Contact</p>
-            </div>
-            <form>
-                <label htmlFor="name">Name</label>
-                <input
-                    name="name"
-                    id="name"
-                    type="text"
-                    placeholder="Enter name... "
-                    required
-                />
-                <label htmlFor="age">Age</label>
-                <input
-                    name="age"
-                    id="age"
-                    type="number"
-                    placeholder="Enter age..."
-                    required
-                />
+            </section>
+            <section className={styled.section}>
+                <form
+                    className={
+                        styled.p + " " + styled["gayathri-bold"] + " " + styled.form
+                    }
+                >
+                    <fieldset>
+                        <label htmlFor="name">
+                            Name{" "}
+                            <input
+                                className={styled.p + " " + styled["gayathri-bold"]}
+                                name="name"
+                                id="name"
+                                type="text"
+                                placeholder="Enter name... "
+                                required
+                            />
+                        </label>
 
-                <label htmlFor="country">Country</label>
-                <input
-                    name="country"
-                    id="country"
-                    type="text"
-                    placeholder="Enter country..."
-                    required
-                />
-                <label htmlFor="email">Email</label>
-                <input
-                    name="email"
-                    id="email"
-                    type="email"
-                    placeholder="Enter email..."
-                    required
-                />
-                <label htmlFor="phoneNumber">Phone Number</label>
-                <input
-                    name="phoneNumber"
-                    id="phoneNumber"
-                    type="number"
-                    placeholder="Enter phone number..."
-                    required
-                />
-                <label htmlFor="comment">Comment</label>
-                <textarea
-                    name="comment"
-                    id="comment"
-                    rows="5"
-                    placeholder="Enter comment..."
-                    required
-                />
-                <label htmlFor="termCondition">
-                    By ticking the box, you agree to these terms and condition of service.
-                </label>
-                <input
-                    name="termCondition"
-                    id="termCondition"
-                    type="checkbox"
-                    required
-                />
-                <input
-                    name="reset"
-                    id="reset"
-                    type="reset"
-                    value="Reset form"
-                    required
-                />
-                <input
-                    name="submit"
-                    id="submit"
-                    type="submit"
-                    value="Submit form"
-                    required
-                />
-            </form>
+                        <label htmlFor="age">
+                            Age{" "}
+                            <input
+                                className={styled.p + " " + styled["gayathri-bold"]}
+                                name="age"
+                                id="age"
+                                type="number"
+                                placeholder="Enter age..."
+                                required
+                            />
+                        </label>
+
+                        <label htmlFor="country">
+                            Country{" "}
+                            <input
+                                className={styled.p + " " + styled["gayathri-bold"]}
+                                name="country"
+                                id="country"
+                                type="text"
+                                placeholder="Enter country..."
+                                required
+                            />
+                        </label>
+
+                        <label htmlFor="email">
+                            Email{" "}
+                            <input
+                                className={styled.p + " " + styled["gayathri-bold"]}
+                                name="email"
+                                id="email"
+                                type="email"
+                                placeholder="Enter email..."
+                                required
+                            />
+                        </label>
+
+                        <label htmlFor="phoneNumber">
+                            Phone Number{" "}
+                            <input
+                                className={styled.p + " " + styled["gayathri-bold"]}
+                                name="phoneNumber"
+                                id="phoneNumber"
+                                type="number"
+                                placeholder="Enter phone number..."
+                                required
+                            />
+                        </label>
+
+                        <label htmlFor="comment">
+                            Comment{" "}
+                            <textarea
+                                className={styled.p + " " + styled["gayathri-bold"]}
+                                name="comment"
+                                id="comment"
+                                rows="3"
+                                cols="25"
+                                placeholder="Enter comment..."
+                                required
+                            />
+                        </label>
+
+                        <label htmlFor="termCondition">
+                            <input
+                                className={styled.p + " " + styled["gayathri-bold"]}
+                                name="termCondition"
+                                id="termCondition"
+                                type="checkbox"
+                                required
+                            />
+                            By ticking the box, you agree to these terms and condition of
+                            service.
+                        </label>
+                        <div>
+                            <input
+                                className={styled.p + " " + styled["gayathri-bold"]}
+                                name="reset"
+                                id="reset"
+                                type="reset"
+                                value="Reset form"
+                                required
+                            />
+                            <input
+                                className={styled.p + " " + styled["gayathri-bold"]}
+                                name="submit"
+                                id="submit"
+                                type="submit"
+                                value="Submit form"
+                                required
+                            />
+                        </div>
+
+                    </fieldset>
+                </form>
+            </section>
         </>
     );
 }
