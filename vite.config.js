442c86c868be7c8e5bc41c3264daa4b5c37b6299
Tai Minh <minhtainth06111999@gmail.com>
2024-04-01 16:34:47 +0700

    demo-project-1

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+import million from "million/compiler"
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    million.vite({
+      auto: {
+        threshold: 0.05,
+        skip: ["useBadHook", /badVariable/g],
+      },
+    }),
+    react(),
+  ],
 })
